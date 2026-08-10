c2989340659c6373a5582981510c15b6afcb36fc
aninatechnologies <aninatechnologies@gmail.com>
2026-08-10 19:54:01 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,18 @@
-const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_NAME = 'permai-court-cache-v1';
 const ASSETS = [
-  './',
-  './index.html'
+  '/permaicourt1/',
+  '/permaicourt1/index.html',
+  '/permaicourt1/manifest.json',
+  '/permaicourt1/icon-192.png',
+  '/permaicourt1/icon-512.png',
+  '/permaicourt1/icon-maskable-512.png'
 ];
 
 // Menyimpan fail semasa proses install
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      // Menggunakan cache.addAll untuk menyimpan semua aset kritikal
       return cache.addAll(ASSETS);
     })
   );
